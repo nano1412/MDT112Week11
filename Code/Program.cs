dd86086b1992de0fee7e835b79e9af185a063739
Nano1412 <nano.khongsri@gmail.com>
2023-04-05 14:43:06 +0700

    add ReversePrintx10 and WWL.txt

---
+++ Code/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program{
+  static void Main(string[] args){    
+    ReversePrintx10();
+  }
+
+  //ArrayDemo_ReversePrintx10--------------------------------------
+  static void ReversePrintx10(){
+    int[] num = new int[10];
+    num[10] = 9;
+    Console.WriteLine(num[10]);
+    for(int i = 0; i <= 9; i++){
+      num[i] = int.Parse(Console.ReadLine());
+    }
+
+    for(int i = 9; i >= 0; i--){
+      Console.WriteLine(num[i]);
+    }
+  }
+  //ArrayDemo_ReversePrintx10-END----------------------------------
+}
