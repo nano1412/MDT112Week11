d4bea2a554d869fefd9b44d2ea0a2d8dcd5bbb7a
Nano1412 <nano.khongsri@gmail.com>
2023-04-05 14:54:59 +0700

    add StudentHeight

--- Code/Program.cs
+++ Code/Program.cs
@@ -2,14 +2,13 @@
 
 class Program{
   static void Main(string[] args){    
-    ReversePrintx10();
+    // ReversePrintx10();
+    StudentHeight();
   }
 
   //ArrayDemo_ReversePrintx10--------------------------------------
   static void ReversePrintx10(){
     int[] num = new int[10];
-    num[10] = 9;
-    Console.WriteLine(num[10]);
     for(int i = 0; i <= 9; i++){
       num[i] = int.Parse(Console.ReadLine());
     }
@@ -19,4 +18,24 @@ class Program{
     }
   }
   //ArrayDemo_ReversePrintx10-END----------------------------------
+
+  //StudentHeight--------------------------------------------------
+  static void StudentHeight(){
+    // declear Array
+    double[] studentHeightcm = new double[15];
+
+    // loop input
+    for(int i = 0; i < 15; i++){
+      Console.Write("Student No.{0} height(cm): ", i+1);
+      studentHeightcm[i] = double.Parse(Console.ReadLine());
+    }
+
+    // ask for which student user want to know
+    Console.Write("student number: ");
+    int wantedstudent = int.Parse(Console.ReadLine());
+
+    // output
+    Console.WriteLine("student No.{0} height = {1} cm", wantedstudent, studentHeightcm[wantedstudent - 1]);
+  }
+  //StudentHeight-END----------------------------------------------
 }
